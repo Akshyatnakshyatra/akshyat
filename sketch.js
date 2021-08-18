183036ac1e1a42e8c0a66bfc078412b42886aa4c
Akshyatnakshyatra <88238152+Akshyatnakshyatra@users.noreply.github.com>
2021-08-18 15:53:19 +0530

    Add files via upload
    c11 project run jaxon

--- sketch.js
+++ sketch.js
@@ -1,42 +1,52 @@
-var ship, shipimg, sea, seaimg1, edges;
+var runner, runnerimg, path, pathimg, edges;
+var invboundary1, invboundary2;
 
 
 function preload(){
-  shipimg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-  seaimg1 = loadImage("sea.png");
+  runnerimg = loadAnimation("Runner-1.png","Runner-2.png");
+  pathimg = loadImage("path.png");
 }
 
 function setup()
 {
-  createCanvas(600,600);
- // creating sea 
-  sea=createSprite(10,10);
-  sea.addImage("seaimage",seaimg1);
-
+  createCanvas(400,400);
  
-  // creating ship
-  ship = createSprite(150,150,50,50);
-  ship.addAnimation("sailing",shipimg);
-  edges = createEdgeSprites();
-  
-  
-  //adding scale and position to ship
-  ship.scale = 0.3;
-  
+  // creating path
+  path=createSprite(200,200);
+  path.addImage("pathimage",pathimg);
+  path.velocityY=4;
+  path.scale=1;
 
+  // creating invisible boundary 1 and 2
+  invboundary1=createSprite(30,0,30,800);
+  invboundary2=createSprite(380,0,30,800);
+  invboundary1.visible=false;
+  invboundary2.visible=false;
+
+
+  // creating runner jackson
+  runner = createSprite(160,300);
+  runner.addAnimation("running",runnerimg);
+  edges = createEdgeSprites();
+    
+  //adding scale  to the  runner
+  
+  runner.scale = 0.1;
   
 }
-
 
 function draw()
 {
   //set background color 
  background("white");
- 
-sea.velocityX=-3;
-if (sea.x<0)
+
+runner.x=World.mouseX;
+runner.collide(invboundary1);
+runner.collide(invboundary2);
+
+if (path.y > 400)
   {
-    sea.x=sea.width/2;
+    path.y=height/2;
   }
   drawSprites();
 }
